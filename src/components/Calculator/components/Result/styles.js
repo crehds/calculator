1fa0d21a2a451e096bec1ca4d54ba79c363da10d
Carlos Rodriguez <carlos15erh@gmail.com>
2022-09-03 16:49:24 -0500

    Add: Result Component

---
+++ src/components/Calculator/components/Result/styles.js
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+
+export const ResultWrapper = styled.div`
+  width: 254px;
+  height: 50px;
+  max-width: 100%;
+  background-color: var(--white);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: var(--gray-600);
+`;
