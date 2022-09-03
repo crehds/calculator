c7e516ecc8b0472d501a987cdac8d1e9a35c0569
Carlos Rodriguez <carlos15erh@gmail.com>
2022-09-03 01:46:41 -0500

    center the calculator

--- src/containers/styles.js
+++ src/containers/styles.js
@@ -3,4 +3,7 @@ import styled from '@emotion/styled';
 export const StyledAppContainer = styled.div`
   min-height: 100vh;
   background-color: rgba(215, 215, 215, 0.95);
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
