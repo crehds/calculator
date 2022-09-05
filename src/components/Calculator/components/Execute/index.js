f443220c50ddcd4b1c11ec7dcee902b70588bb5b
Carlos Rodriguez <carlos15erh@gmail.com>
2022-09-04 20:53:53 -0500

    refactor execute component

--- src/components/Calculator/components/Execute/index.js
+++ src/components/Calculator/components/Execute/index.js
@@ -2,34 +2,31 @@ import { Check } from '../../../Icons/Check';
 import { Equal } from '../../../Icons/Equal';
 import { Key } from '../Key';
 
-export const Execute = ({ executeButton, resultHandler }) => {
+const configHandler = (executeButton) => {
   switch (executeButton) {
-    case 'check':
-      return (
-        <Key
-          resultHandler={resultHandler}
-          color='cyan-500'
-          column={[5, 6]}
-          row={[4, 6]}
-          type='future'
-        >
-          <Check />
-        </Key>
-      );
     case 'equal':
-      return (
-        <Key
-          resultHandler={resultHandler}
-          color='cyan-500'
-          column={[5, 6]}
-          row={[4, 6]}
-          type='execute'
-        >
-          <Equal />
-        </Key>
-      );
-
+      return {
+        type: 'execute',
+        children: <Equal />,
+      };
     default:
-      break;
+      return {
+        type: 'future',
+        children: <Check />,
+      };
   }
+};
+export const Execute = ({ executeButton, resultHandler }) => {
+  const { type, children } = configHandler(executeButton);
+  return (
+    <Key
+      resultHandler={resultHandler}
+      color={'cyan-500'}
+      column={[5, 6]}
+      row={[4, 6]}
+      type={type}
+    >
+      {children}
+    </Key>
+  );
 };
